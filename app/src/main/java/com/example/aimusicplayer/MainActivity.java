76129e109f7af24463253ca3b4431de9126ad7fe
Utsav-Katuwal-6548 <>
2020-04-15 21:54:49 +1000

    Fourth Commit

--- app/src/main/java/com/example/aimusicplayer/MainActivity.java
+++ app/src/main/java/com/example/aimusicplayer/MainActivity.java
@@ -10,12 +10,15 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +41,61 @@ public class MainActivity extends AppCompatActivity {
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results)
+            {
+                ArrayList<String> matchesFound = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                if (matchesFound != null)
+                {
+                    keeper = matchesFound.get(0);
+                    Toast.makeText(MainActivity.this, "Results = " + keeper, Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
         //it will listen the touch on any part of the screen since it's a Relative Layout
         parentRelativeLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -47,9 +105,12 @@ public class MainActivity extends AppCompatActivity {
                 {
                     //action down refers to long press on screen
                     case MotionEvent.ACTION_DOWN:
+                        speechRecognizer.startListening(speechRecognizerIntent);
+                        keeper = "";
                         break;
                     //action up refers to single press on screen
                     case MotionEvent.ACTION_UP:
+                        speechRecognizer.stopListening();
                         break;
                 }
                 return false;
